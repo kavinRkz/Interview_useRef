0d8afc268e43fe2a97e2ca73066e1fcd5ee884f4
kavinRkz <git@stackblitz.com>
2026-02-12 20:15:37 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Interview_useRef
+
+[Edit in StackBlitz next generation editor ⚡️](https://stackblitz.com/~/github.com/kavinRkz/Interview_useRef)
---
+++ src/App.js
@@ -0,0 +1,20 @@
+import React, { useRef } from 'react';
+import './style.css';
+
+export default function App() {
+  const myRef = useRef('');
+  const handleAdd = () => {
+    console.log(myRef.current);
+    if (myRef.current.value.length > 4) {
+      // myRef.current.style.color = 'red';
+      myRef.current.style.backgroundColor = 'blue';
+      myRef.current.focus();
+    }
+  };
+  return (
+    <div>
+      <input type="text" ref={myRef} />
+      <button onClick={handleAdd}>Add</button>
+    </div>
+  );
+}
